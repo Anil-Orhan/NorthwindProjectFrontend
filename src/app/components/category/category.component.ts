5c210f50fdf01446b8a5bbf48d8847d6562dadb0
Anıl Orhan <anilorhanmail@gmail.com>
2022-03-11 18:44:18 +0300

    11/03/2022 18-44

---
+++ src/app/components/category/category.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { Category } from 'src/app/models/category';
+
+import { CategoryService } from 'src/app/services/category.service';
+
+@Component({
+  selector: 'app-category',
+  templateUrl: './category.component.html',
+  styleUrls: ['./category.component.css'],
+})
+export class CategoryComponent implements OnInit {
+  categories: Category[] = [];
+  dataLoaded: boolean = false;
+  currentCategory: Category = { categoryId: 0, categoryName: '' };
+
+  constructor(private categoryService: CategoryService) {}
+  ngOnInit(): void {
+    this.getCategories();
+  }
+
+  getCategories() {
+    this.categoryService.getCategories().subscribe((response) => {
+      this.categories = response.data;
+      this.dataLoaded = true;
+    });
+  }
+  setCurrentCategory(c: Category) {
+    this.currentCategory = c;
+    console.log(c.categoryName);
+  }
+
+  getCurrentCategoryClass(c: Category) {
+    if (c == this.currentCategory) {
+      return 'list-group-item active';
+    } else {
+      return 'list-group-item';
+    }
+  }
+}
