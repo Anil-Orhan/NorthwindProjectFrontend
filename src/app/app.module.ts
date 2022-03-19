2d9b04ddd2e81f9f31a919ea11ced05c0c3ce00d
Anıl Orhan <anilorhanmail@gmail.com>
2022-03-20 01:06:40 +0300

    20/03 yedek

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,7 @@ import { VatAddedPipe } from './pipes/vat-added.pipe';
 import { FilterPipePipe } from './pipes/filter-pipe.pipe';
 
 import { ToastrModule } from 'ngx-toastr';
+import { CartSummaryComponent } from './components/cart-summary/cart-summary.component';
 @NgModule({
   declarations: [
     AppComponent,
@@ -21,6 +22,7 @@ import { ToastrModule } from 'ngx-toastr';
     NaviComponent,
     VatAddedPipe,
     FilterPipePipe,
+    CartSummaryComponent,
   ],
   imports: [
     BrowserModule,
@@ -29,6 +31,10 @@ import { ToastrModule } from 'ngx-toastr';
     FormsModule,
     ToastrModule.forRoot({
       positionClass: 'toast-bottom-right',
+      maxOpened: 5,
+      timeOut: 1500,
+      closeButton: true,
+      easeTime: 1000,
     }),
     BrowserAnimationsModule,
   ],
--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { Product } from 'src/app/models/product';
+import { CartService } from 'src/app/services/cart.service';
 
 import { ProductService } from 'src/app/services/product.service';
 
@@ -18,7 +19,8 @@ export class ProductComponent implements OnInit {
   constructor(
     private productService: ProductService,
     private activatedRoute: ActivatedRoute,
-    private toasterService: ToastrService
+    private toasterService: ToastrService,
+    private cartService: CartService
   ) {}
 
   ngOnInit(): void {
@@ -46,6 +48,7 @@ export class ProductComponent implements OnInit {
       });
   }
   addToCart(product: Product) {
+    this.cartService.addToCart(product);
     this.toasterService.success('Sepete Eklendi! ', product.productName);
   }
 }
